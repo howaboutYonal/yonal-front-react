8c98fe5f2f83df5c477d9bddd5a9ef18b256fa6b
ksh04023 <ksh04023@gmail.com>
2021-02-13 20:42:52 +0900

    create config.js

--- config/config.js
+++ config/config.js
@@ -1,23 +1,26 @@
-{
+require('dotenv').config();
+
+module.exports = {
   "development": {
     "username": "admin_yonal",
-    "password": "yonal20210202_dsbjh",
+    "password": process.env.DATABASE_PW,
     "database": "yonal",
     "host": "admin-yonal.ci04dcmjvtyq.ap-northeast-2.rds.amazonaws.com",
     "dialect": "mysql"
   },
   "test": {
     "username": "admin_yonal",
-    "password": "yonal20210202_dsbjh",
+    "password": process.env.DATABASE_PW,
     "database": "yonal",
     "host": "admin-yonal.ci04dcmjvtyq.ap-northeast-2.rds.amazonaws.com",
     "dialect": "mysql"
   },
   "production": {
     "username": "admin_yonal",
-    "password": "yonal20210202_dsbjh",
+    "password": process.env.DATABASE_PW,
     "database": "yonal",
     "host": "admin-yonal.ci04dcmjvtyq.ap-northeast-2.rds.amazonaws.com",
     "dialect": "mysql"
   }
 }
+};
