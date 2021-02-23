568bf0e5998477e0cddd7faa5caf2e327d3657aa
suebeen <1003jamie@naver.com>
2021-02-23 12:46:20 +0900

    Update Main.js

--- client/src/screen/HomePages/Main.js
+++ client/src/screen/HomePages/Main.js
@@ -1,22 +1,29 @@
 import React, { useState } from 'react';
 import {Link} from 'react-router-dom'
+import axios from 'axios'
 
 const Main = ({location}) => {
     const myName = location.name;
     const myEmail = location.email;
     const myImg = location.image;
-    const [myProjects, ] = useState('1');
+    const [myProjects, setMyProjects] = useState([]);
+
+    axios.post('http://localhost:5000/v1/get/myProject', {
+        email: myEmail
+    }).then(function(res){
+        // [{"projectId":1},{"projectId":413153}]
+        setMyProjects(res.data.projectId);
+    });
 
     return (
         <div>
-            <div>
-                {myName}
-                <br></br>
-                <img className='profile' src={myImg}/>
-            </div>
+            <div><img className='profile' src={myImg}/></div>
             <div className='logoText'>내 프로젝트</div>
+            <div>
+                {myProjects}
+            </div>
             <Link to='./totalcal'>
-                <button className = 'indexBtn' projectId={myProjects}>아직미완</button>
+                <button className = 'indexBtn' projectId="1">아직미완</button>
             </Link>
             <Link to='./create'>
                 <button className = 'indexBtn'>프로젝트 만들기</button>
