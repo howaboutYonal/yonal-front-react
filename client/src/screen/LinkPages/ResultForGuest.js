a404a4d8ee87dbf5a7831409cce60aa7610b6cbd
gildong <gildong@asd.com>
2021-02-25 02:14:16 +0900

    resultforguest user 분리

--- client/src/screen/LinkPages/ResultForGuest.js
+++ client/src/screen/LinkPages/ResultForGuest.js
@@ -17,11 +17,13 @@ const useStyles = makeStyles((thems) =>{
 
 const ResultForGuest = () => {
     const [value, ] = useState(new Date());
-    const [apiData, setApiData] = useState([
-        {
-           votedata:""
-        }
-    ]);
+    const [apiData, setApiData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(async () =>{
+        setIsLoading(false);
+        await fetchApi().then(res => setApiData(res)).then(setIsLoading(true));    
+    },[]);
 
     function parse(str) {
         var y = str.substr(0,4);
@@ -30,58 +32,64 @@ const ResultForGuest = () => {
         var r = new Date(y,m-1,d);
         return  r;
     };
-
+    
     function jointpars(apiData){
         var voteData = JSON.parse(apiData.votedata);
-        console.log(apiData);
-        voteData = voteData[0].filter(function (element){
-            if (element) return element.date;
-        });
-        var joint = [];
-        for( var i= 0;i<voteData.length;i++){
-            joint = [
-                ...joint,
+
+        var totaljoint = [];
+        for(var j=0; j<voteData.length;j++){
+            var joint = [];
+            for( var i= 0;i<voteData[j].length;i++){
+                joint = [
+                    ...joint,
+                    {
+                        votedata:voteData[j][i].date
+                    }
+                ]
+            }
+            totaljoint=[
+                ...totaljoint,
                 {
-                    votedata:voteData[i].date
+                    votedata:joint
                 }
             ]
         }
+        console.log(totaljoint);
 
-        return joint;
+        return totaljoint;
     }
     
-    useEffect(async () =>{
-        var link = document.location.href;
-        var urlforshareLink = 'http://localhost:5000/v1/get/project-result-from-link';
-        await fetchApi(urlforshareLink,link).then(res => jointpars(res)).then(res => setApiData(res)).then(res=> console.log(apiData));
-    },[]);
-
-    function fetchApi(url, param){
-        return axios.post(url, {shareLink:param}).then(function (res) {
-            return res.data;
+    async function fetchApi(){
+        return axios.post('http://localhost:5000/v1/get/project-result-from-link', {shareLink:document.location.href}).then(function (res) {
+            return jointpars(res.data);
         })
-    }
-
-
+    }    
+    
     function tileClassName(params){
-        if(params.view === 'month' && !(apiData.length ===0))
-        if(apiData.some(x => parse(x.votedata.replaceAll('-','')).valueOf() === params.date.valueOf())){
-            console.log(params);
-            return 'selected_day';
-        }
+        if(!(apiData.length===0))
+            for(var i=0;i<apiData.length;i++)
+                return comparFerUser(params,i);
     }
 
+    function comparFerUser(params,idx){
+        if(params.view === 'month' && !(apiData[idx].length ===0))
+            if(apiData[idx].votedata.some(x => parse(x.votedata.replaceAll('-','')).valueOf() === params.date.valueOf())){
+                console.log(params,'finish');
+                return 'selected_day';
+            }
+    }
+ 
     return (
         <div>
             <h1>종합된 날짜</h1>
             <div>
-            {apiData ?<div>
+            {isLoading ?
             <Calendar className="calendar"
                 value = {value}
                 minDate = {value}
-                tileClassName = {tileClassName} 
-            /> </div>
-            :  "Loading"
+                tileClassName = {tileClassName } 
+            />
+            : "Loading"
             }
             </div>
         </div>
