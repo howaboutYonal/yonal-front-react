646c22a67977a8ed8502c5955aa5976d254f6888
gildong <gildong@asd.com>
2021-02-26 01:30:55 +0900

    result final!!

--- client/src/screen/HomePages/ResultForManager.js
+++ client/src/screen/HomePages/ResultForManager.js
@@ -4,12 +4,14 @@ import {Link} from 'react-router-dom'
 import 'react-calendar/dist/Calendar.css';
 import '../calendar.css';
 import axios from 'axios';
-import { ListItemSecondaryAction } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles'
 
 /*  참여한 모든 유저의 date데이터를 api로 불러온다.
     이렇게 불러온 데이터를 종합하여 캘린더에 출력한다. */
 
 // props를 통해 projectID를 전달받은 후 해당 프로젝트 반환
+let useStyles = makeStyles({ foo: props => ({backgroundColor: props.backgroundColor, }),}); 
+const rgbHex = require('rgb-hex');
 
 function getUUID() { // UUID v4 generator in JavaScript (RFC4122 compliant)
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
@@ -19,49 +21,75 @@ function getUUID() { // UUID v4 generator in JavaScript (RFC4122 compliant)
 }
 
 const ResultForManager = ({location}) => {
+    let classes0 = useStyles({ backgroundColor: '#'+rgbHex(40,75,114, 0)});
+    let classes1 = useStyles({ backgroundColor: '#'+rgbHex(40,75,114, 0.1)});
+    let classes2 = useStyles({ backgroundColor: '#'+rgbHex(40,75,114, 0.2)});
+    let classes3 = useStyles({ backgroundColor: '#'+rgbHex(40,75,114, 0.3)});
+    let classes4 = useStyles({ backgroundColor: '#'+rgbHex(40,75,114, 0.4)});
+    let classes5 = useStyles({ backgroundColor: '#'+rgbHex(40,75,114, 0.5)});
+    let classes6 = useStyles({ backgroundColor: '#'+rgbHex(40,75,114, 0.6)});
+    let classes7 = useStyles({ backgroundColor: '#'+rgbHex(40,75,114, 0.7)});
+    let classes8 = useStyles({ backgroundColor: '#'+rgbHex(40,75,114, 0.8)});
+    let classes9 = useStyles({ backgroundColor: '#'+rgbHex(40,75,114, 0.9)});
+    let classes10 = useStyles({ backgroundColor: '#'+rgbHex(40,75,114, 1)});
+
+    const select = [ classes0, classes1, classes2, classes3, classes4, classes5, classes6, classes7, classes8, classes9, classes10 ]
+    const [weight, setWeight] = useState();
+
     const [value, ] = useState(new Date());
-    const [apiData, setApiData] = useState([
-        {
-           votedata:""
-        }
-    ]);
+    const [apiData, setApiData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [flag, setFlag] = useState(false);
+
     const [shareLink,] = useState('http://localhost:3000/share/'+getUUID());
+
+    useEffect(async () =>{
+        if(flag) setFlag(false);
+        setIsLoading(false);
+        await fetchApi().then(res => setApiData(res)).then(setIsLoading(true));
+    },[]);
+    useEffect(()=>{
+        if(isLoading) setFlag(true);
+    },[isLoading]);
 
     function parse(str) {
         var y = str.substr(0,4);
         var m = str.substr(4,2);
-        var d = str.substr(6,2);
-        var r = new Date(y,m-1,d);
-        return  r;
+        var d = str.substr(6,2); 
+        return  new Date(y,m-1,d);
     };
+    
 
     function jointpars(apiData){
         var voteData = JSON.parse(apiData.votedata);
-        voteData = voteData[0].filter(function (element){
-            if (element) return element.date;
-        });
 
-        var joint = [];
-        for( var i= 0;i<voteData.length;i++){
-            joint = [
-                ...joint,
+        var totaljoint = [];
+        for(var j=0; j<voteData.length;j++){
+            var joint = [];
+            for( var i= 0;i<voteData[j].length;i++){
+                joint = [
+                    ...joint,
+                    {
+                        votedata:voteData[j][i].date
+                    }
+                ]
+            }
+            totaljoint=[
+                ...totaljoint,
                 {
-                    votedata:voteData[i].date
+                    votedata:joint
                 }
             ]
         }
-        return joint;
-    }
-    
-    useEffect(async () =>{
-        await fetchApi().then(res => jointpars(res)).then(res => setApiData(res));
-    },[]);
 
-    function fetchApi(){
-        var url = 'http://localhost:5000/v1/get/project-result';
-        return axios.post(url, {projectId:location.projectId}).then(function (res) {
-            return res.data;
-        })
+        return totaljoint;
+    }
+
+    async function fetchApi(){
+        return axios.post('http://localhost:5000/v1/get/project-result', {projectId:location.projectId}).then(function (res) {
+            setWeight((select.length/res.data.count));
+            return jointpars(res.data);
+        });
     }
     function fetchApi2(){
         var url = 'http://localhost:5000/v1/save/shareLink';
@@ -72,22 +100,36 @@ const ResultForManager = ({location}) => {
 
 
     function tileClassName(params){
-        if(params.view === 'month' && !(apiData.length ===0))
-        if(apiData.some(x => parse(x.votedata.replaceAll('-','')).valueOf() === params.date.valueOf()))
-            return 'selected_day';
+        if(!(apiData.length===0)){
+            var count = 0;
+            apiData.map(i=>{ count += comparFerUser(params,i) ? 1 : 0; });
+            return assignColor(count);
+        }
+    }
+
+    function assignColor(count){
+        count = weight*count > select.length-1 ? select.length-1 : weight*count;
+        try { return `${select[Math.round(count)].foo}`;
+        } catch (error) { console.log(weight,Math.round(count),'err'); }
+    }
+
+    function comparFerUser(params,idx){
+        if(params.view === 'month' && idx)
+            if(idx.votedata.some(x => parse(x.votedata.replaceAll('-','')).valueOf() === params.date.valueOf()))
+                return true;
     }
 
     return (
         <div>
             <h1>종합된 날짜</h1>
             <div>
-            {apiData ?<div>
+            {isLoading ?
             <Calendar className="calendar"
                 value = {value}
                 minDate = {value}
-                tileClassName = {tileClassName} 
-            /> </div>
-            :  "Loading"
+                tileClassName = {flag?tileClassName:"" } 
+            />
+            : "Loading"
             }
             </div>
             <Link to={{pathname: './copylink', value: shareLink}}>
--- client/src/screen/LinkPages/ResultForGuest.js
+++ client/src/screen/LinkPages/ResultForGuest.js
@@ -4,43 +4,44 @@ import 'react-calendar/dist/Calendar.css';
 import '../calendar.css';
 import axios from 'axios';
 import { makeStyles } from '@material-ui/core/styles'
-import { ListItemSecondaryAction } from '@material-ui/core';
 
 /*  참여한 모든 유저의 date데이터를 api로 불러온다.
     이렇게 불러온 데이터를 종합하여 캘린더에 출력한다. */
 
 // projectId는 링크를 통해 유추한다.
 
-const useStyles = makeStyles({
-    foo: props => ({
-        backgroundColor: props.backgroundColor,
-    }),
-    bar: {
-        // CSS property
-        color: props => props.color,
-    },
-});
-    
-
+let useStyles = makeStyles({ foo: props => ({backgroundColor: props.backgroundColor, }),}); 
+const rgbHex = require('rgb-hex');
 
 const ResultForGuest = () => {
-    const props = { backgroundColor: 'black', color: '#284b72' };
-    const classes = useStyles(props);
+    let classes0 = useStyles({ backgroundColor: '#'+rgbHex(40,75,114, 0)});
+    let classes1 = useStyles({ backgroundColor: '#'+rgbHex(40,75,114, 0.1)});
+    let classes2 = useStyles({ backgroundColor: '#'+rgbHex(40,75,114, 0.2)});
+    let classes3 = useStyles({ backgroundColor: '#'+rgbHex(40,75,114, 0.3)});
+    let classes4 = useStyles({ backgroundColor: '#'+rgbHex(40,75,114, 0.4)});
+    let classes5 = useStyles({ backgroundColor: '#'+rgbHex(40,75,114, 0.5)});
+    let classes6 = useStyles({ backgroundColor: '#'+rgbHex(40,75,114, 0.6)});
+    let classes7 = useStyles({ backgroundColor: '#'+rgbHex(40,75,114, 0.7)});
+    let classes8 = useStyles({ backgroundColor: '#'+rgbHex(40,75,114, 0.8)});
+    let classes9 = useStyles({ backgroundColor: '#'+rgbHex(40,75,114, 0.9)});
+    let classes10 = useStyles({ backgroundColor: '#'+rgbHex(40,75,114, 1)});
+
+    const select = [ classes0, classes1, classes2, classes3, classes4, classes5, classes6, classes7, classes8, classes9, classes10 ]
+    const [weight, setWeight] = useState();
 
     const [value, ] = useState(new Date());
     const [apiData, setApiData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [flag, setFlag] = useState(false);
+
     useEffect(async () =>{
         if(flag) setFlag(false);
         setIsLoading(false);
-        await fetchApi().then(res => setApiData(res)).then(setIsLoading(true));   
+        await fetchApi().then(res => setApiData(res)).then(setIsLoading(true));
     },[]);
     useEffect(()=>{
         if(isLoading) setFlag(true);
     },[isLoading]);
-
-
 
     function parse(str) {
         var y = str.substr(0,4);
@@ -70,28 +71,35 @@ const ResultForGuest = () => {
                 }
             ]
         }
-        console.log(totaljoint);
 
         return totaljoint;
     }
     
     async function fetchApi(){
         return axios.post('http://localhost:5000/v1/get/project-result-from-link', {shareLink:document.location.href}).then(function (res) {
+            setWeight((select.length/res.data.count));
             return jointpars(res.data);
-        })
+        });
     }    
     
     function tileClassName(params){
-        if(!(apiData.length===0))
-            return apiData.map(i=>comparFerUser(params,i));
+        if(!(apiData.length===0)){
+            var count = 0;
+            apiData.map(i=>{ count += comparFerUser(params,i) ? 1 : 0; });
+            return assignColor(count);
+        }
+    }
+
+    function assignColor(count){
+        count = weight*count > select.length-1 ? select.length-1 : weight*count;
+        try { return `${select[Math.round(count)].foo}`;
+        } catch (error) { console.log(weight,Math.round(count),'err'); }
     }
 
     function comparFerUser(params,idx){
         if(params.view === 'month' && idx)
-            if(idx.votedata.some(x => parse(x.votedata.replaceAll('-','')).valueOf() === params.date.valueOf())){
-                console.log(params,'finish',idx,flag);
-                return `${classes.foo} ${classes.bar}`;
-            }
+            if(idx.votedata.some(x => parse(x.votedata.replaceAll('-','')).valueOf() === params.date.valueOf()))
+                return true;
     }
  
     return (
