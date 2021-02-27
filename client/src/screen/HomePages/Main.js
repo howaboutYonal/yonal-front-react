2080e5eca59f814918cb3a63fe2cff34d3791b96
suebeen <1003jamie@naver.com>
2021-02-27 21:11:30 +0900

    finish

--- client/src/screen/HomePages/Main.js
+++ client/src/screen/HomePages/Main.js
@@ -1,33 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import {Link} from 'react-router-dom'
 import axios from 'axios'
-import ProjectButton from '../../component/ProjectButton'
 
 const Main = ({location}) => {
     const myName = location.name;
     const myEmail = location.email;
     const myImg = location.image;
-    const myProjects = [];
-    const [flag, setFlag] = useState(false);
-    const [isLoading, setIsLoading] = useState(false);
-    
-    function setMyProjects(arr) {
+    const [myProjects, setMyProjects] = useState([]);
+    const projectList = myProjects.map(item => 
+        <div>
+            <Link to={{pathname:'./totalcal', projectId:item.id}}>
+                <button>{item.name}</button>
+            </Link>
+        </div>
+    );
+
+    function copyData(arr) {
+        const temp = [];
         arr.forEach(function(item){
             var id = item['projectId'];
             var name = item['Projects.name'];
-            myProjects.push({id, name});
+            temp.push({id, name});
         })
+        setMyProjects(temp);
     }
 
     useEffect(async () =>{
-        if(flag) setFlag(false);
-        await fetchApi().then(res => setMyProjects(res.data.projectData)).then(setIsLoading(true));
-        console.log(myProjects);
+        await fetchApi().then(res => copyData(res.data.projectData));
     },[])
-
-    useEffect(()=>{
-        if(isLoading) setFlag(true);
-    },[isLoading]);
     
     async function fetchApi(){
         return axios.post('http://localhost:5000/v1/get/myProject', {
@@ -42,9 +42,7 @@ const Main = ({location}) => {
                 <button className = 'indexBtn'>프로젝트 만들기</button>
             </Link>
             <div className='logoText'>내 프로젝트</div>
-            {isLoading ?
-            <ProjectButton value={myProjects}/> : "loading"
-            }
+            {projectList}
         </div>
     );
 }
