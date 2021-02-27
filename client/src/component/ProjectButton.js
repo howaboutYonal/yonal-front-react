10ab94c5ef6d3ff4f51260f289f85588c64aec7d
suebeen <1003jamie@naver.com>
2021-02-27 20:56:18 +0900

    Update ProjectButton.js

--- client/src/component/ProjectButton.js
+++ client/src/component/ProjectButton.js
@@ -3,18 +3,27 @@ import {Link} from 'react-router-dom'
 
 const ProjectButton = (props) => {
     
-    const myProjects = props.value;
-    console.log(myProjects)
+    // temp data  ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ내꺼에서 받아옴..
+    const myProjects = [
+        {id: 1, name: "yonal 설연휴 회의 일정"},
+        {id: 413153, name: "프로젝트 생성 api적용 테스트!"},
+        {id: 413160, name: "projectUser test"},
+        {id: 413161, name: "휴.."},
+        {id: 413162, name: "어렵네"}
+    ];
+    // const myProjects = props.value;
+
+    const projectList = myProjects.map(item => 
+        <div>
+            <Link to={{pathname:'./totalcal', projectId:item.id}}>
+                <button>{item.name}</button>
+            </Link>
+        </div>
+    );
 
     return (
-        
         <div>
-            <Link to={{pathname:'./totalcal', projectId:0}}>
-                <button className = 'btn' >프로젝트 이름</button>
-            </Link>
-            {/* <Link to={{pathname:'./totalcal', projectId:myProjects[0].id}}>
-                <button className = 'btn' >{myProjects[0].name}</button>
-            </Link> */}
+            {projectList}
         </div>
     );
 }
